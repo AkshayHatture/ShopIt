bbdf9426ffc366f51f2343e71b44b90cfad01b10
Akshay Hatture <akshayhatture11@gmail.com>
2025-07-10 20:44:07 +0530

    Update authSlice.js

--- frontend/src/redux/slice/authSlice.js
+++ frontend/src/redux/slice/authSlice.js
@@ -34,7 +34,7 @@ export const loginUser = createAsyncThunk(
 
       return response.data.user;
     } catch (error) {
-      return rejectWithVlaue(error.response.data);
+      return rejectWithValue(error.response.data);
     }
   }
 );
@@ -53,7 +53,7 @@ export const registerUser = createAsyncThunk(
 
       return response.data.user;
     } catch (error) {
-      return rejectWithVlaue(error.response.data);
+      return rejectWithValue(error.response.data);
     }
   }
 );
